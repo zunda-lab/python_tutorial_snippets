27120c8e8721dd5e34c6e644663e93ea08e6b50a
Koji ONO <naninuneter55@gmail.com>
2024-11-30 04:36:32 +0900

    for some reason

--- notebook/introduction.py
+++ notebook/introduction.py
@@ -298,12 +298,20 @@ a, b = 0, 1
 while a < 10:
     print(a)
     a, b = b, a+b
+# 0
+# 1
+# 1
+# 2
+# 3
+# 5
+# 8
 
 i = 256*256
 print('The value of i is', i)
+# The value of i is 65536
 
 a, b = 0, 1
 while a < 1000:
     print(a, end=',')
     a, b = b, a+b
-
+# 0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,
